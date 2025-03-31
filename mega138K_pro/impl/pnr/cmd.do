9dcfae3365a4db3f2cc0ed77090a8c941eb310ed
Lathe <n11akira@yahoo.co.jp>
2025-04-01 00:55:45 +0900

    0401

--- mega138K_pro/impl/pnr/cmd.do
+++ mega138K_pro/impl/pnr/cmd.do
@@ -1,9 +1,9 @@
--d C:\Git\TangPrimer20K_LUT-Network\mega138K_pro\impl\gwsynthesis\top.vg
+-d H:\git\Gowin_LUT-Network\mega138K_pro\impl\gwsynthesis\top.vg
 -p GW5AST-138B-FCPBGA676A-1
 -pn GW5AST-LV138FPG676AES
--cst C:\Git\TangPrimer20K_LUT-Network\mega138K_pro\src\top.cst
--cfg C:\Git\TangPrimer20K_LUT-Network\mega138K_pro\impl\pnr\device.cfg
--sdc C:\Git\TangPrimer20K_LUT-Network\mega138K_pro\src\lcd.sdc
+-cst H:\git\Gowin_LUT-Network\mega138K_pro\src\top.cst
+-cfg H:\git\Gowin_LUT-Network\mega138K_pro\impl\pnr\device.cfg
+-sdc H:\git\Gowin_LUT-Network\mega138K_pro\src\lcd.sdc
 -bit
 -tr
 -ph
@@ -12,7 +12,6 @@
 -oreg_not_in_iob
 -ioreg_not_in_iob
 -cst_error
--convert_sdp32_36_to_sdp16_18
 -correct_hold 1
 -route_maxfan 23
 -global_freq 100.000
--- mega138K_pro/src/top.v
+++ mega138K_pro/src/top.v
@@ -33,7 +33,7 @@ module top(
     output wire       O_tmds_clk_n    ,
     output wire[2:0]  O_tmds_data_p   ,      //{r,g,b}
     output wire[2:0]  O_tmds_data_n   ,
-    output wire[4:0]  state_led
+    output wire[5:0]  state_led
 );
 
 //memory interface
@@ -81,9 +81,9 @@ assign cmos_pwdn  = 1'b0;
 assign cmos_rst_n = 1'b1;
 assign write_data = {cmos_16bit_data[4:0],cmos_16bit_data[10:5],cmos_16bit_data[15:11]};
 
-//assign state_led[2] = lcd_vs_cnt[4];
-//assign state_led[1] = rst_n; //复位指示灯
-//assign state_led[0] = init_calib_complete; //DDR3初始化指示灯
+assign state_led[2] = camera_de;
+assign state_led[1] = rst_n; //复位指示灯
+assign state_led[0] = init_calib_complete; //DDR3初始化指示灯
 
 reg [4:0] lcd_vs_cnt;
 always@(posedge lcd_vs) lcd_vs_cnt <= lcd_vs_cnt + 1;
@@ -305,47 +305,6 @@ wire init_calib_complete;
 		.I_init_calib_complete(init_calib_complete) // DDR3 controller calibration completed
 	);
 
-/*
-Video_Frame_Buffer_Top Video_Frame_Buffer_Top_inst
-( 
-    .I_rst_n              (init_calib_complete ),//rst_n            ),
-    .I_dma_clk            (dma_clk          ),   //sram_clk         ),
-`ifdef USE_THREE_FRAME_BUFFER 
-    .I_wr_halt            (1'd0             ), //1:halt,  0:no halt
-    .I_rd_halt            (1'd0             ), //1:halt,  0:no halt
-`endif
-    // video data input             
-//    .I_vin0_clk           (cmos_16bit_clk_half   ),
-    .I_vin0_clk           (cmos_16bit_clk   ),
-    .I_vin0_vs_n          (~cmos_vsync      ),//只接收负极性
-    .I_vin0_de            (cmos_16bit_wr    ),
-    .I_vin0_data          (write_data       ),
-    .O_vin0_fifo_full     (                 ),
-
-    // video data output            
-    .I_vout0_clk          (video_clk        ),
-    .I_vout0_vs_n         (~syn_off0_vs     ),//只接收负极性
-    .I_vout0_de           (camera_de        ),
-    .O_vout0_den          (off0_syn_de      ),
-    .O_vout0_data         (off0_syn_data    ),
-    .O_vout0_fifo_empty   (                 ),
-    // ddr write request
-    .I_cmd_ready          (cmd_ready          ),
-    .O_cmd                (cmd                ),//0:write;  1:read
-    .O_cmd_en             (cmd_en             ),
-    .O_app_burst_number   (app_burst_number   ),
-    .O_addr               (addr               ),//[ADDR_WIDTH-1:0]
-    .I_wr_data_rdy        (wr_data_rdy        ),
-    .O_wr_data_en         (wr_data_en         ),//
-    .O_wr_data_end        (wr_data_end        ),//
-    .O_wr_data            (wr_data            ),//[DATA_WIDTH-1:0]
-    .O_wr_data_mask       (wr_data_mask       ),
-    .I_rd_data_valid      (rd_data_valid      ),
-    .I_rd_data_end        (rd_data_end        ),//unused 
-    .I_rd_data            (rd_data            ),//[DATA_WIDTH-1:0]
-    .I_init_calib_complete(init_calib_complete)
-);*/ 
-
 
 localparam N = 7; //delay N clocks
                           
@@ -422,52 +381,6 @@ assign lcd_dclk    			  = video_clk;     //video_clk_phs;
 		.IO_ddr_dqs_n(ddr_dqs_n) //inout [3:0] IO_ddr_dqs_n
 	);
 
-/*
-DDR3MI DDR3_Memory_Interface_Top_inst 
-(
-    .clk                (video_clk          ),
-    .memory_clk         (memory_clk         ),
-    .pll_lock           (DDR_pll_lock       ),
-    .rst_n              (rst_n              ), //rst_n
-    .app_burst_number   (app_burst_number   ),
-    .cmd_ready          (cmd_ready          ),
-    .cmd                (cmd                ),
-    .cmd_en             (cmd_en             ),
-    .addr               (addr               ),
-    .wr_data_rdy        (wr_data_rdy        ),
-    .wr_data            (wr_data            ),
-    .wr_data_en         (wr_data_en         ),
-    .wr_data_end        (wr_data_end        ),
-    .wr_data_mask       (wr_data_mask       ),
-    .rd_data            (rd_data            ),
-    .rd_data_valid      (rd_data_valid      ),
-    .rd_data_end        (rd_data_end        ),
-    .sr_req             (1'b0               ),
-    .ref_req            (1'b0               ),
-    .sr_ack             (                   ),
-    .ref_ack            (                   ),
-    .init_calib_complete(init_calib_complete),
-    .clk_out            (dma_clk            ),
-    .burst              (1'b1               ),
-    // mem interface
-    .ddr_rst            (                 ),
-    .O_ddr_addr         (ddr_addr         ),
-    .O_ddr_ba           (ddr_bank         ),
-    .O_ddr_cs_n         (ddr_cs         ),
-    .O_ddr_ras_n        (ddr_ras        ),
-    .O_ddr_cas_n        (ddr_cas        ),
-    .O_ddr_we_n         (ddr_we         ),
-    .O_ddr_clk          (ddr_ck          ),
-    .O_ddr_clk_n        (ddr_ck_n        ),
-    .O_ddr_cke          (ddr_cke          ),
-    .O_ddr_odt          (ddr_odt          ),
-    .O_ddr_reset_n      (ddr_reset_n      ),
-    .O_ddr_dqm          (ddr_dm           ),
-    .IO_ddr_dq          (ddr_dq           ),
-    .IO_ddr_dqs         (ddr_dqs          ),
-    .IO_ddr_dqs_n       (ddr_dqs_n        )
-);*/
-
     // -----------------------------
     //  表示画像オーバーレイ
     // -----------------------------
@@ -528,8 +441,6 @@ DDR3MI DDR3_Memory_Interface_Top_inst
         end
     end
 
-
-
 //==============================================================================
 //TMDS TX(HDMI4)
 wire serial_clk;
@@ -558,25 +469,5 @@ wire serial_clk;
 		.O_tmds_data_p(O_tmds_data_p), //output [2:0] O_tmds_data_p
 		.O_tmds_data_n(O_tmds_data_n) //output [2:0] O_tmds_data_n
 	);
-
-/*
-DVI_TX_Top DVI_TX_Top_inst
-(
-    .I_rst_n       (hdmi4_rst_n   ),  //asynchronous reset, low active
-    .I_serial_clk  (serial_clk    ),
-
-    .I_rgb_clk     (lcd_dclk      ),  //pixel clock
-    .I_rgb_vs      (lcd_vs        ), 
-    .I_rgb_hs      (lcd_hs        ),    
-    .I_rgb_de      (lcd_de        ), 
-    .I_rgb_r       ( off0_syn_de? {off0_syn_data[4:0],3'b0}: bin_en?{8{bin_view}}: mnist_en? {8{mnist_view}}: dvi_x),  //tp0_data_r
-    .I_rgb_g       ( off0_syn_de? {off0_syn_data[10:5],2'b0}: bin_en?{8{bin_view}}: mnist_en? {8{mnist_view}}: dvi_y),  //,  
-    .I_rgb_b       ( off0_syn_de? {off0_syn_data[15:11],3'b0}: bin_en?{8{bin_view}}: mnist_en? {8{mnist_view}}: 8'hff),  //,
-
-    .O_tmds_clk_p  (O_tmds_clk_p  ),
-    .O_tmds_clk_n  (O_tmds_clk_n  ),
-    .O_tmds_data_p (O_tmds_data_p ),  //{r,g,b}
-    .O_tmds_data_n (O_tmds_data_n )
-);*/
 
 endmodule
